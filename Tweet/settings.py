6452a45a22c6006e9f0a5b346cbaf074424d4eea
aakashsach <297aakash@gmail.com>
2020-11-15 17:48:56 -0600

    init

--- Tweet/settings.py
+++ Tweet/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6-4e$2d7$wd!l12&m0^t628*)*i515rv!_hbo180)gkph6v$l#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','.cfe.sh']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Tweets',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'Tweet.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,"templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
