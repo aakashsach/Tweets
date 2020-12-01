ad14bffaa754c6d498eab5cdb33a22d6570ee870
aakashsach <297aakash@gmail.com>
2020-12-01 16:15:16 +0530

    Added like, unlike, and retweet functionality

--- Tweet/settings.py
+++ Tweet/settings.py
@@ -27,8 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','.cfe.sh']
 LOGIN_URL = "/login"
-
-
+MAX_TWEET_LENGTH=240
+TWEET_ACTION_OPTIONS = ["like", "unlike", "retweet"]
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third-party
+    'rest_framework',
+    # internal
     'Tweets',
 ]
 
@@ -120,3 +123,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DEFAULT_RENDERER_CLASSES = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
+
+if DEBUG:
+    DEFAULT_RENDERER_CLASSES += [
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+REST_FRAMEWORK = {
+    
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication'
+    ],
+    'DEFAULT_RENDERER_CLASSES': DEFAULT_RENDERER_CLASSES
+}
